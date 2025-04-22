98a43c0a945ef96a730bc6fc7b539396153bf3cc
Sienna Baird <siennatanner21@gmail.com>
2025-04-22 16:13:29 -0700

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Sienna Tanner on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
