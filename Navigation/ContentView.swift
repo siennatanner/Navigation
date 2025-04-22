63900718e33a3889836e2e36e4a6a0a36cdba99f
Sienna Baird <siennatanner21@gmail.com>
2025-04-22 16:25:13 -0700

    Added text to the root view and NavigationLink to second view

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -9,13 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack {
+            VStack {
+                Text("This is the root view 🌳")
+                NavigationLink(destination:  Text("You've arrived to the Second View 🎊")) {
+                    Text("Click me!")
+                }
+            }
         }
-        .padding()
+        
+        
     }
 }
 
